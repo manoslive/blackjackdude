579a175eff772f9d10054e107dad5abf6deffc84
ChuckCooper <shaun.11.cooper@gmail.com>
2015-03-25 12:18:57 -0400

    paquet de cartes

---
+++ blackjack/PaquetCartes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    class PaquetCartes
+    {
+        public static Random rnd = new Random();
+        public List<string> Paquet = new List<string>();
+
+        public PaquetCartes()
+        {
+            //string str = string.Empty;
+            //str = lst.OrderBy(xx => rnd.Next()).First();
+            //return str;
+            Paquet.Add("ace_of_clubs");
+            Paquet.Add("ace_of_diamonds");
+            Paquet.Add("ace_of_hearts");
+            Paquet.Add("ace_of_spades");
+            Paquet.Add("2_of_clubs");
+            Paquet.Add("2_of_diamonds");
+            Paquet.Add("2_of_hearts");
+            Paquet.Add("2_of_spades");
+            Paquet.Add("3_of_clubs");
+            Paquet.Add("3_of_diamonds");
+            Paquet.Add("3_of_hearts");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+            Paquet.Add("");
+        }
+    }
+}
