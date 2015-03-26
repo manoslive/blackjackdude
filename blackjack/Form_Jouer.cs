8d137406304c813a26f3c3fa63a8392773386485
ChuckCooper <shaun.11.cooper@gmail.com>
2015-03-26 15:59:24 -0400

    asd

--- blackjack/Form_Jouer.cs
+++ blackjack/Form_Jouer.cs
@@ -114,6 +114,12 @@ namespace blackjack
             if (LB_Points_J1.Text.Length > 0)
                 Points = Convert.ToInt32(LB_Points_J1.Text);
             LB_Points_J1.Text = (Points + lePaquet.GetValeur()).ToString();
+
+            if (Convert.ToInt32(LB_Points_J1.Text) > 21)
+            {
+                MessageBox.Show("Vous avez busté! \nJoueur 2 à gagné");
+                DisableButtons();
+            }
         }
         public void PigerCarteJ1()
         {
@@ -164,6 +170,11 @@ namespace blackjack
             if (LB_Points_J2.Text.Length > 0)
                 Points = Convert.ToInt32(LB_Points_J2.Text);
             LB_Points_J2.Text = (Points + lePaquet.GetValeur()).ToString();
+            if(Convert.ToInt32(LB_Points_J2.Text) > 21)
+            {
+                MessageBox.Show("Vous avez busté! \nJoueur 1 à gagné");
+                DisableButtons();
+            }
         }
         public void PigerCarteJ2()
         {
