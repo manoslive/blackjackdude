c28c4019bb6fc9f6169855d907d77dbe07c388d5
ChuckCooper <shaun.11.cooper@gmail.com>
2015-03-27 11:09:31 -0400

    enlever pb

--- blackjack/Form_Jouer.Designer.cs
+++ blackjack/Form_Jouer.Designer.cs
@@ -46,11 +46,9 @@
             this.FB_PasserJ2 = new FlashButton.FlashButton();
             this.FB_PigerJ2 = new FlashButton.FlashButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.PB_Banner = new System.Windows.Forms.PictureBox();
             this.GB_OptionJoueur2.SuspendLayout();
             this.GB_OptionJoueur1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.PB_Banner)).BeginInit();
             this.SuspendLayout();
             // 
             // LB_Joueur1
@@ -264,24 +262,11 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
-            // PB_Banner
-            // 
-            this.PB_Banner.BackColor = System.Drawing.Color.Transparent;
-            this.PB_Banner.BackgroundImage = global::blackjack.Properties.Resources.bannerResultat;
-            this.PB_Banner.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.PB_Banner.Location = new System.Drawing.Point(-4, 106);
-            this.PB_Banner.Name = "PB_Banner";
-            this.PB_Banner.Size = new System.Drawing.Size(564, 229);
-            this.PB_Banner.TabIndex = 9;
-            this.PB_Banner.TabStop = false;
-            this.PB_Banner.Visible = false;
-            // 
             // Form_Jouer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(563, 413);
-            this.Controls.Add(this.PB_Banner);
             this.Controls.Add(this.LB_Points_J2);
             this.Controls.Add(this.LB_Points_J1);
             this.Controls.Add(this.BTN_Annuler);
@@ -305,7 +290,6 @@
             this.GB_OptionJoueur2.ResumeLayout(false);
             this.GB_OptionJoueur1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.PB_Banner)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -331,6 +315,5 @@
         private FlashButton.FlashButton FB_PigerJ2;
         private FlashButton.FlashButton FB_PasserJ1;
         private FlashButton.FlashButton FB_PigerJ1;
-        private System.Windows.Forms.PictureBox PB_Banner;
     }
 }
